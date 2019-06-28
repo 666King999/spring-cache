2789abf7966afb88e44fedd4bad958e594949908
666king999 <1039273400@qq.com>
2019-06-28 15:00:06 +0800

    che first

---
+++ src/main/java/com/atguigu/springche/mapper/DepartmentMapper.java
@@ -0,0 +1,8 @@
+package com.atguigu.springche.mapper;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.web.bind.annotation.Mapping;
+
+
+public interface DepartmentMapper {
+}
